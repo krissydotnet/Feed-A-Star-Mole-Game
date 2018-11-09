6a75895b2aab09f44aace0a24d5557373bf0d492
Krissy Dukes <krissydotnet@yahoo.com>
2018-11-09 10:21:24 -0500

    Update to game

--- script/mole.js
+++ script/mole.js
@@ -1,8 +1,33 @@
 
+const moles = document.querySelectorAll('.mole');
+const gamescreen = document.querySelector('.background');
+const winnerscreen = document.querySelector('.winner');
+const worm = document.querySelector('.worm-container');
+let score = 0;
+let royalcount = 0;
+
 function setupMoles(moles) {
     let molesArray = [];
     let molesObj = {};
     for (let i = 0; i < moles.length; i++) {
+            moles[i].addEventListener("click", function() {
+                if (moles[i].classList.contains("royal")) {
+                    score = score + 2;
+                    moles[i].classList.add('fed-royal');      
+                    moles[i].classList.add('leaving-royal');
+                    
+                } else {
+                    score++;
+                    moles[i].classList.add('fed-mole');                 
+                }
+                
+                worm.style.width = (score * 10) + '%';
+                if (score === 10) {
+                    gamescreen.classList.add('hide');
+                    winnerscreen.classList.remove('hide');
+                }
+            });
+
             moles[i].classList.add('hide');
             molesObj.state = "hungry"
             molesObj.lastAppeared =  Date.now();
@@ -12,25 +37,34 @@ function setupMoles(moles) {
 }
 
 function displayMoles(moles, molesArray) {
+    
 for (let i = 0; i < moles.length; i++) {
     let randomNumber = Math.random();
     if (randomNumber > .5) {
         moles[i].classList.add('hide');
     } else {
+        moles[i].removeAttribute("class");
+        moles[i].classList.add('mole');
+        royalcount++;
+        if (royalcount === 10) {
+            moles[i].classList.add('royal');
+            royalcount = 0;
+        } else {
+            
+        }
         moles[i].classList.remove('hide');
         molesArray[i].lastAppeared = Date.now();
     }
 }
-console.log(molesArray);
+
 }
 
 function init() {
-    const moles = document.querySelectorAll('.mole');
+
   
     const molesArray =  setupMoles(moles);
-    console.log(molesArray);
-    setInterval(function() {displayMoles(moles, molesArray)}, 2000)
-    
+    setInterval(function() {displayMoles(moles, molesArray)}, 2000);
+
 }
 
 init();
